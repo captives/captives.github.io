d5901137367127912a88642299a17ec7d9dccd98
seven <593278382@qq.com>
2020-04-22 15:07:04 +0800

    测试

--- src/webrtc/views/web/index.vue
+++ src/webrtc/views/web/index.vue
@@ -1,7 +1,14 @@
 <template>
   <el-main>
-    <el-row v-for="(item, index) in list" :key="item.label" :label="item.label">
-      <el-button :type="index%2 ==0 ?'danger':'success'" @click="loadURL(item.url)">{{item.label}}</el-button>
+    <el-row :gutter="20">
+      <el-col
+        v-for="(item, index) in list"
+        :key="item.label"
+        :label="item.label"
+        :span="list.length/24"
+      >
+        <el-button :type="index%2 ==0 ?'danger':'success'" @click="loadURL(item.url)">{{item.label}}</el-button>
+      </el-col>
     </el-row>
     <el-main style="height: 80%">
       <iframe :src="url" allow="autoplay" frameborder="0" scrolling="no"></iframe>
@@ -43,8 +50,8 @@ export default {
   },
   mounted() {
     //媒体参与度查询 chrome://media-engagement/
-    this.list.push({ label: '自动播放策略,直接加载', url: "/v3/testIframe.html", desc: "这里模拟百家云教室加载课件" });
-    this.list.push({ label: '自动播放+自动跳转', url: "/v3/testReward.html", desc: "这里模拟百家云教室加载课件" });
+    this.list.push({ label: '自动播放 - 直接加载', url: "/v3/testIframe.html", desc: "这里模拟百家云教室加载课件" });
+    this.list.push({ label: '自动播放 + 自动跳转', url: "/v3/testReward.html", desc: "这里模拟百家云教室加载课件" });
   }
 }
 </script>
@@ -63,14 +70,12 @@ export default {
         height: 100%;
       }
     }
-
-    
   }
 
   iframe {
-      width: 100%;
-      height: 100%;
-      border: none;
-    }
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
 }
 </style>
