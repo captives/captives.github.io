fca2a35a47bdb0073557698277aff9cfc5664876
shuyang.zhao <shuyang.zhao@onesmart.org>
2021-05-25 12:00:12 +0800

    srv

--- src/charts/views/About.vue
+++ src/charts/views/About.vue
@@ -1,5 +1,9 @@
 <template>
     <el-main>
+        <p>client</p>
+        <vue-source src="/charts/views/Xterm.vue" lang="html"></vue-source>
+        <p>server</p>
+        <vue-source src="srv/socket/xterm.js" lang="html"></vue-source>
     </el-main>
 </template>
 
